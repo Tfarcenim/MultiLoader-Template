a9bb0fd25a085f9172fce8e78910be72e8c645cb
Tfarcenim <44327798+Tfarcenim@users.noreply.github.com>
2024-05-18 23:28:14 -0400

    add pack format

--- common/src/main/java/com/example/examplemod/CommonClass.java
+++ common/src/main/java/com/example/examplemod/CommonClass.java
@@ -1,7 +1,7 @@
 package com.example.examplemod;
 
 import com.example.examplemod.platform.Services;
-import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.Registry;
 import net.minecraft.world.item.Items;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,7 +22,7 @@ public class CommonClass {
     public static void init() {
 
         LOG.info("Hello from Common init on {}! we are currently in a {} environment!", Services.PLATFORM.getPlatformName(), Services.PLATFORM.getEnvironmentName());
-        LOG.info("The ID for diamonds is {}", BuiltInRegistries.ITEM.getKey(Items.DIAMOND));
+        LOG.info("The ID for diamonds is {}", Registry.ITEM.getKey(Items.DIAMOND));
 
         // It is common for all supported loaders to provide a similar feature that can not be used directly in the
         // common code. A popular way to get around this is using Java's built-in service loader feature to create
